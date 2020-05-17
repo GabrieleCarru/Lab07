229452095d127b981d0dee846f64d6a767aade10
gabrielecarru <gabrielecarru@localhost>
2020-05-17 12:00:22 +0200

    Creata interfaccia grafica

--- Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/FXMLController.java
+++ Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/FXMLController.java
@@ -5,8 +5,17 @@
 package it.polito.tdp.poweroutages;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.poweroutages.model.Model;
+import it.polito.tdp.poweroutages.model.Nerc;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
@@ -16,8 +25,47 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML // fx:id="cmcNERC"
+    private ComboBox<Nerc> cmbNERC; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMaxYears"
+    private TextField txtMaxYears; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMaxHours"
+    private TextField txtMaxHours; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnWorstCase"
+    private Button btnWorstCase; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+    
+    private Model model;
+
+    @FXML
+    void analysisWorstCase(ActionEvent event) {
 
     }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert cmbNERC != null : "fx:id=\"cmbNERC\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMaxYears != null : "fx:id=\"txtMaxYears\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMaxHours != null : "fx:id=\"txtMaxHours\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnWorstCase != null : "fx:id=\"btnWorstCase\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+        txtResult.setStyle("-fx-font-family: monospace");
+
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
+    	setComboBox();
+    }
+
+	private void setComboBox() {
+		List<Nerc> nercList = model.getNercList();
+		cmbNERC.getItems().addAll(nercList);
+	}
 }
--- Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/model/Model.java
+++ Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.poweroutages.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.polito.tdp.poweroutages.DAO.PowerOutageDAO;
@@ -7,9 +8,11 @@ import it.polito.tdp.poweroutages.DAO.PowerOutageDAO;
 public class Model {
 	
 	PowerOutageDAO podao;
+	private List<Nerc> nercList;
 	
 	public Model() {
 		podao = new PowerOutageDAO();
+		nercList = new ArrayList<>(podao.getNercList());
 	}
 	
 	public List<Nerc> getNercList() {
